d5d3e05d54f1f2878748b0d61a440eb39dbbf338
Palesa <palesagaetsewe52@gmail.com>
2025-10-05 01:01:35 +0200

    fixed i think

--- app/src/main/java/com/example/budgettrain/WelcomeActivity.kt
+++ app/src/main/java/com/example/budgettrain/WelcomeActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
 
         val loginButton = findViewById<MaterialButton>(R.id.loginButton)
         val registerButton = findViewById<MaterialButton>(R.id.registerButton)
